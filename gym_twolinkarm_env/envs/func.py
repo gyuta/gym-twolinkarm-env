32efe930a1a264723ed96745936238f828b40c79
gyuta <kuroshiba0408@gmail.com>
2021-11-01 10:55:28 +0900

    modify target point

--- gym_twolinkarm_env/envs/func.py
+++ gym_twolinkarm_env/envs/func.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def new_target():
     while True:
-        x = np.random.random(2) * 2 - 1
-        if np.sum(x**2) < 1:
+        x = np.random.random(2) * 4 - 2
+        if np.sum(x**2) < 8:
             break
     return x
